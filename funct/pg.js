4db68e6fe25e598c9f57beb04ae3d1845a4283ea
DamanmayStenV2225 <130616339+DamanmayStenV2225@users.noreply.github.com>
2026-01-05 15:15:15 +0530

    Add files via upload

---
+++ funct/pg.js
@@ -0,0 +1,17 @@
+const con = document.querySelector(".pc");
+
+for (let i = 1; i <= 12; i++) {
+  const img = document.createElement("img");
+  img.loading = "lazy";
+  img.src = `images/img${i}.jpg`;
+  con.appendChild(img);
+}
+
+const bp = document.createElement("p");
+bp.className = "border";
+bp.textContent = "For more.";
+con.appendChild(bp);
+        
+const ad = document.createElement("div");
+ad.className = "ad-slot";
+con.appendChild(ad);
